a881162674fdb6f75bb18a3d1df01b2ef490e0a3
Paolo Angeli <pangeli70@gmail.com>
2023-03-27 02:58:03 +0200

    Cleaning deps

--- deps.ts
+++ deps.ts
@@ -4,10 +4,6 @@
  * ------------------------------------------------------------------------
  */
 
-// https://deno.land/std
-export * as StdFs from "https://deno.land/std@0.153.0/fs/mod.ts";
-export * as StdPath from "https://deno.land/std@0.153.0/path/mod.ts";
-export * as StdColors from "https://deno.land/std@0.153.0/fmt/colors.ts";
 
 // https://github
 export * as Uts from "https://raw.githubusercontent.com/Pangeli70/apg-uts/master/mod.ts";
